a64f1454c8cc535b9be2878d7b7131a7d446cd47
Matutozi <sobowalegz2@gmail.com>
2024-05-15 21:28:14 +0100

    task 1: mongodb  utils

---
+++ utils/db.js
@@ -0,0 +1,32 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+    constructor() {
+        const host = process.env.DB_HOST || 'localhost';
+        const port = process.env.DB_PORT || 27017;
+        const database = process.env.DB_DATABASE || 'files_manager';
+        const url = `mongodb://${host}:${port}`;
+
+        MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+            if (err) {
+                this.dbClient = false;
+            } else {
+                this.dbClient = client.db(database);
+            }
+        });
+    }
+
+    isAlive() {
+        return !!this.dbClient;
+    }
+
+    async nbUsers() {
+        return this.dbClient.collection('users').countDocuments();
+    }
+
+    async nbFiles() {
+        return this.dbClient.collection('files').countDocuments();
+    }
+}
+
+export default new DBClient();
